3adb65053a5da8c8573985a73cc8b803e1e910b6
jvitormelo <jvitor.ovidio2@gmail.com>
2023-03-04 23:10:55 -0300

    feat: delete workorder

---
+++ src/api/work-orders/use-delete-work-order-mutation.ts
@@ -0,0 +1,23 @@
+import { toast } from "@/infra/toast";
+import { httpClient } from "@/infra/http-client";
+import { queryClient } from "@/infra/query-client";
+import { WorkOrder } from "@/types/entities/workorders";
+import { useMutation } from "@tanstack/react-query";
+
+const deleteWorkOrder = async (id: number) => {
+  return httpClient.delete(`/workorders/${id}`);
+};
+
+export const useDeleteWorkOrderMutation = () => {
+  return useMutation(deleteWorkOrder, {
+    onSuccess: (_, id) => {
+      toast.success("Work order deleted successfully");
+      queryClient.setQueryData<WorkOrder[]>(["workorders"], (oldData) => {
+        return oldData?.filter((workOrder) => workOrder.id !== id);
+      });
+    },
+    onError: () => {
+      toast.error("Error deleting work order");
+    },
+  });
+};
